29b81c6c599f327dc97042c44b39132ffa1fe4fb
FX科技 <11727850+fuxing2022@user.noreply.gitee.com>
2023-09-01 11:17:03 +0800

    添加注释

--- example/main.go
+++ example/main.go
@@ -68,7 +68,7 @@ func DBConfig() {
 func init() {
 	//初始化人事数据
 	RoleUser["人事主管"] = []string{"张经理"}
-	RoleUser["老板"] = []string{"李老板"}
+	RoleUser["老板"] = []string{"李老板","老板娘"}
 	RoleUser["副总"] = []string{"赵总", "钱总", "孙总"}
 }
 
