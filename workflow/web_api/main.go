294ddfc7a5f9d540c55883d3c8b9f5330ac135da
FX科技 <11727850+fuxing2022@user.noreply.gitee.com>
2023-09-05 09:53:00 +0800

    修改启动函数参数

--- workflow/web_api/main.go
+++ workflow/web_api/main.go
@@ -4,17 +4,18 @@ import (
 	. "github.com/Bunny3th/easy-workflow/workflow/web_api/router"
 	"github.com/gin-gonic/gin"
 )
+
 /*开启工作流引擎WebApi	参数说明:
 engine: *gin.Engine gin框架(github.com/gin-gonic/gin)
 GinMode可选项: debug | release
 addr 监听地址与端口,如"localhost:8080"
- */
-func StartWebApi(engine *gin.Engine, GinMode string, addr string) {
-	e := NewRouter(engine, GinMode)
+*/
+func StartWebApi(engine *gin.Engine, BaseUrl string, ShowSwaggerDoc bool, addr string) {
+	e := NewRouter(engine, BaseUrl, ShowSwaggerDoc)
 	e.Run(addr)
 
 	//如何使用swagger生成文档
 	//一般在main包所在目录执行 swag init
 	//但本项目中，swagger命令需要在web_api目录中加上-d参数执行，如下
-	//swag init -d ./,../model
+	//swag init --instanceName easy-workflow -d ./,../model
 }
--- workflow/web_api/router/routers.go
+++ workflow/web_api/router/routers.go
@@ -8,30 +8,29 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func NewRouter(engine *gin.Engine,GinMode string) *gin.Engine {
-	gin.SetMode(GinMode)
+func NewRouter(engine *gin.Engine,BaseUrl string,ShowSwaggerDoc bool) *gin.Engine {
+	router:=engine.Group(BaseUrl)
 
-	//只有在debug模式下才开启swagger
-	if GinMode == "debug" {
-		engine.GET("/process/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	if ShowSwaggerDoc {
+		router.GET("/process/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
 
-	engine.POST("/process/def/save",ProcDef_Save)
-	engine.GET("/process/def/list",ProcDef_ListBySource)
-	engine.GET("/process/def/get",ProcDef_GetProcDefByID)
+	router.POST("/process/def/save",ProcDef_Save)
+	router.GET("/process/def/list",ProcDef_ListBySource)
+	router.GET("/process/def/get",ProcDef_GetProcDefByID)
 
-	engine.POST("/process/inst/start",ProcInst_Start)
-	engine.POST("/process/inst/revoke",ProcInst_Revoke)
-	engine.GET("/process/inst/task_history",ProcInst_TaskHistory)
+	router.POST("/process/inst/start",ProcInst_Start)
+	router.POST("/process/inst/revoke",ProcInst_Revoke)
+	router.GET("/process/inst/task_history",ProcInst_TaskHistory)
 
-	engine.POST("/process/task/pass",Task_Pass)
-	engine.POST("/process/task/pass/directly",Task_Pass_DirectlyToWhoRejectedMe)
+	router.POST("/process/task/pass",Task_Pass)
+	router.POST("/process/task/pass/directly",Task_Pass_DirectlyToWhoRejectedMe)
 
-	engine.POST("/process/task/reject",Task_Reject)
-	engine.POST("/process/task/reject/free",Task_FreeRejectToUpstreamNode)
-	engine.GET("/process/task/todo",Task_ToDoList)
-	engine.GET("/process/task/finished",Task_FinishedList)
-	engine.GET("/process/task/upstream",Task_UpstreamNodeList)
+	router.POST("/process/task/reject",Task_Reject)
+	router.POST("/process/task/reject/free",Task_FreeRejectToUpstreamNode)
+	router.GET("/process/task/todo",Task_ToDoList)
+	router.GET("/process/task/finished",Task_FinishedList)
+	router.GET("/process/task/upstream",Task_UpstreamNodeList)
 
 	return engine
 }
