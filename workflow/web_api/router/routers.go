53d5817cccdfe5f6f98f42001ebdf10341164c3d
兔老三 <11727850+fuxing2022@user.noreply.gitee.com>
2023-09-12 10:09:46 +0800

    添加swagger标题、说明

--- workflow/web_api/router/routers.go
+++ workflow/web_api/router/routers.go
@@ -1,22 +1,26 @@
 package router
 
 import (
-	_ "github.com/Bunny3th/easy-workflow/workflow/web_api/docs" // 导入swagger文档用的
+	 "github.com/Bunny3th/easy-workflow/workflow/web_api/docs" // 导入swagger文档用的
 	. "github.com/Bunny3th/easy-workflow/workflow/web_api/service"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func NewRouter(engine *gin.Engine, ApiBaseUrl string, ShowSwaggerDoc bool, SwaggerUrl string) *gin.Engine {
+func NewRouter(engine *gin.Engine, ApiBasePath string, ShowSwaggerDoc bool, SwaggerUrl string) *gin.Engine {
 	//注意，由于我们执行swag init的时候指定了InstanceName，所以这里也必须传入InstanceName
 	if ShowSwaggerDoc {
 		engine.GET(SwaggerUrl, ginSwagger.WrapHandler(swaggerFiles.Handler, func(c *ginSwagger.Config) {
 			c.InstanceName = "easyworkflow"
 		}))
 	}
+	//swagger信息设置
+	docs.SwaggerInfoeasyworkflow.BasePath=ApiBasePath
+	docs.SwaggerInfoeasyworkflow.Title="Easy WorkFlow接口说明"
+	docs.SwaggerInfoeasyworkflow.Description="https://github.com/Bunny3th/easy-workflow"
 
-	router := engine.Group(ApiBaseUrl)
+	router := engine.Group(ApiBasePath)
 
 	router.POST("/def/save", ProcDef_Save)
 	router.GET("/def/list", ProcDef_ListBySource)
