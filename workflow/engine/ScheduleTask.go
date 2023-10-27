e19a030c2da9f67078bd24232905bf5c30cf298a
兔老三 <11727850+fuxing2022@user.noreply.gitee.com>
2023-10-27 14:36:07 +0800

    添加任务计划功能

--- workflow/engine/ScheduleTask.go
+++ workflow/engine/ScheduleTask.go
@@ -1,36 +1,88 @@
 package engine
 
 import (
-	"log"
+	"errors"
+	"fmt"
 	"time"
 )
 
-//记录任务运行状态
-var ScheduleRecorder map[string]string
+//计划任务池
+//任务计划池的作用是存储任务运行中的信息，方便排查问题
+var ScheduledTaskPool = make(map[string]*ScheduledTask)
 
+//被计划的任务
+type ScheduledTask struct {
+	StartAt        time.Time     //任务开始时间
+	StopAt         time.Time     //任务结束时间
+	IntervalSecond int64         //重复执行间隔(秒),最小1秒
+	Func           func() error  //需要运行的任务方法
+	LastRunTime    time.Time     //上一次运行时间
+	LastResult     string        //上一次运行结果
+	LastDuration   time.Duration //上一次运行时长
+}
 
+//获取任务信息
+func GetScheduledTaskList() map[string]*ScheduledTask {
+	return ScheduledTaskPool
+}
 
-//执行任务计划 参数说明:
+//登记计划任务，任务会被添加进计划任务池，并进入运行状态，可使用GetScheduledTaskList查看任务运行信息
+//**注意**:请使用go关键词运行此函数。因为任务运行周期可能很长，若不使用goroutine运行,则可能造成主进程堵塞
+//参数说明:
 //StartAt 任务开始时间
 //StopAt  任务结束时间
-//IntervalSecond 重复执行间隔(秒)
-//Func 需要执行的方法
-func RegisterScheduleTask(StartAt time.Time, StopAt time.Time, IntervalSecond int64, Func func()) {
+//IntervalSecond 重复执行间隔(秒),最小1秒
+//Func 需要执行的方法,签名必须是func() error
+//
+func ScheduleTask(TaskName string, StartAt time.Time, StopAt time.Time, IntervalSecond int64, Func func() error) error {
+	if _, ok := ScheduledTaskPool[TaskName]; ok {
+		return errors.New("此任务已被加入任务池，无需重复操作")
+	}
+
 	//当前时间
 	now := time.Now()
 
 	//如果当前时间已经在StopAt之后，则不必执行
 	if now.After(StopAt) {
-		return
+		return errors.New("任务结束时间小于当前时间，任务不会被运行")
 	}
 
-	//间隔不能小于1秒
+	//间隔小于1秒会出错
 	if IntervalSecond < 1 {
-		return
+		return errors.New("重复执行间隔最小1秒")
 	}
 
 	//结束时间应在开始时间之后
 	if StopAt.Before(StartAt) {
+		return errors.New("开始时间应小于结束时间")
+	}
+
+	ScheduledTaskPool[TaskName] = &ScheduledTask{
+		StartAt:        StartAt,
+		StopAt:         StopAt,
+		IntervalSecond: IntervalSecond,
+		Func:           Func,
+	}
+
+	//开始运行任务
+	runScheduledTask(TaskName)
+
+	return nil
+}
+
+//运行任务计划
+func runScheduledTask(TaskName string) {
+	//如果计划任务池中没有，则直接退出
+	task, ok := ScheduledTaskPool[TaskName]
+	if !ok {
+		return
+	}
+
+	//当前时间
+	now := time.Now()
+
+	//如果当前时间已经在StopAt之后，则不必执行
+	if now.After(task.StopAt) {
 		return
 	}
 
@@ -38,10 +90,10 @@ func RegisterScheduleTask(StartAt time.Time, StopAt time.Time, IntervalSecond in
 	var waitDuration time.Duration
 
 	//如果当前时间已经在StartAt之后，定时器马上开启
-	if now.After(StartAt) {
+	if now.After(task.StartAt) {
 		waitDuration = 0
 	} else { //否则，设置等待时间
-		waitDuration = StartAt.Sub(now)
+		waitDuration = task.StartAt.Sub(now)
 	}
 
 	//定义一个定时器
@@ -52,29 +104,45 @@ func RegisterScheduleTask(StartAt time.Time, StopAt time.Time, IntervalSecond in
 	<-timer.C
 
 	//定义一个周期性定时器
-	ticker := time.NewTicker(time.Duration(IntervalSecond) * time.Second)
+	ticker := time.NewTicker(time.Duration(task.IntervalSecond) * time.Second)
 	defer ticker.Stop()
+
+	//防止panic导致程序退出
+	defer func() {
+		if err := recover(); err != nil {
+			//记录运行状态
+			task.LastResult = fmt.Sprint(err)
+			//记录运行耗时
+			task.LastDuration = time.Now().Sub(task.LastRunTime)
+		}
+	}()
 
 	for {
 		<-ticker.C
-		//如果当前已经在StopAt之后，则退出
-		if time.Now().After(StopAt) {
+		//如果已经不在计划任务池中，则不需要再运行。
+		_, ok := ScheduledTaskPool[TaskName]
+		if !ok {
 			return
 		}
 
-		//运行任务
-		go func() {
-			//防止panic导致程序退出。任务不应为几次panic而停止运行
-			defer func() {
-				if err := recover(); err != nil {
-					log.Println("计划任务执行中出现异常:", err)
-				}else{
-					ScheduleRecorder[""]=""
-				}
-			}()
+		//如果当前已经在StopAt之后，则退出
+		if time.Now().After(task.StopAt) {
+			return
+		}
 
-			//运行任务
-			Func()
-		}()
+		//记录运行时间
+		task.LastRunTime = time.Now()
+
+		//运行任务 这里不再使用协程。
+		//考虑一个情况:任务间隔2秒，但是A协程运行时间长达10秒，2秒后B又开始跑，这样前面还没结束后面就开始，会造成大量的性能消耗与不可控情况
+		err := task.Func()
+		//记录运行状态
+		if err == nil {
+			task.LastResult = "ok"
+		} else {
+			task.LastResult = err.Error()
+		}
+		//记录运行耗时
+		task.LastDuration = time.Now().Sub(task.LastRunTime)
 	}
 }
